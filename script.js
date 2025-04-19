b3aad607f2c532954c51f677bc234a0e118239a7
miwanoff <marina.iva.noffa@gmail.com>
2025-04-19 11:18:29 +0300

    Add timer

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let timeElement = document.getElementById("time");
-let n = 0;
+// let n = 0;
+
 function time() {
-    timeElement.innerHTML = n;
-    n++;
+  // timeElement.innerHTML = n;
+  // n++;
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+//   if (s < 10) {
+//     s = "0" + s;
+//   }
+  s = s < 10 ? "0" + s : s;
+  timeElement.innerHTML = `<span>${h}</span>:<span>${m}</span>:<span>${s}</span>`;
 }
+
 let t = setInterval(time, 1000);
